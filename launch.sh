c28a4417366a919552c8b3a73734694bb783d3bb
KG <kilian_kegel@hotmail.com>
2020-07-21 17:06:23 +0200

    ### 20200721/1

--- launch.sh
+++ launch.sh
@@ -1,1 +1,8 @@
+YOCTORPI4_ROOT=$PWD
+export YOCTORPI4_ROOT
 source poky-dunfell/oe-init-build-env ./build
+echo ++++++++++++++++++++++++++++++++++++++++++
+echo + run 'bitbake console-image'
+echo +   or
+echo + run 'bitbake qt5-image'
+echo ++++++++++++++++++++++++++++++++++++++++++
